6720bf55cf78a776dd04d81b73448811972eedce
Tomiris-lab1112 <tom_oraz@kbtu.kz>
2025-09-26 10:44:09 +0500

    lab2 4

--- Lab2/5.py
+++ Lab2/5.py
@@ -33,3 +33,41 @@ set1 = {"abc", 34, True, 40, "male"}
 myset = {"apple", "banana", "cherry"}
 print(type(myset))
 """
+
+"""
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
+"""
+
+#Python - Access Tuple Items
+"""
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+"""
+
+"""
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+"""
+
+"""
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+"""
+
+"""
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+"""
+
+"""
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+"""
+
+"""
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+print("Yes, 'apple' is in the fruits tuple")
+"""
+
